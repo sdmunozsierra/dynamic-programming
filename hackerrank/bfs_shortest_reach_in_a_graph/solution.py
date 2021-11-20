6dbf6d757b925fcdf05b5608aa060dc273c076cd
Sergio Sierra <sergiosierra@cleta.io>
2021-11-20 18:01:15 -0500

    Added first hackerrank problem

---
+++ hackerrank/bfs_shortest_reach_in_a_graph/solution.py
@@ -0,0 +1,9 @@
+t = int(input())
+for i in range(t):
+    n,m = [int(value) for value in input().split()]
+    graph = Graph(n)
+    for i in range(m):
+        x,y = [int(x) for x in input().split()]
+        graph.connect(x-1,y-1) 
+    s = int(input())
+    graph.find_all_distances(s-1)
